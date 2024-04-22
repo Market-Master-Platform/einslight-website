bd9686e9389e820e5da21e534b6cc41864163e61
Minh Long <minhlong.wb@gmail.com>
2024-04-22 16:27:46 +0700

    Placeholder

---
+++ pages/blogs/index.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function Blogs() {
+  return (
+    <main>
+      {/* <Navbar /> */}
+      <section className="flex justify-center items-center px-16 py-20 w-full text-center bg-neutral-800 max-md:px-5 max-md:max-w-full">
+        <div className="flex flex-col mt-11 mb-7 max-w-full w-[778px] max-md:mt-10">
+          <h1 className="text-6xl font-extrabold text-white leading-[72px] max-md:max-w-full max-md:text-4xl max-md:leading-[58px]">
+            The news that surrounds us is collected here
+          </h1>
+          <p className="mt-16 text-xl font-medium leading-9 text-gray-400 max-md:mt-10 max-md:max-w-full">
+            Read watch and absorb the content that we publish in our news blog.
+            Also leave comments and communicate with other users.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
