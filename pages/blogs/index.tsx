54109649fd106ebad0e60fb454b624a81a9bca21
Minh Long <minhlong.wb@gmail.com>
2024-04-22 17:06:47 +0700

    Styling for pages

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,9 +1,8 @@
-import Navbar from "@/components/Navbar/Navbar";
+import "../globals.css";
 
 export default function Blogs() {
   return (
     <main>
-      {/* <Navbar /> */}
       <section className="flex justify-center items-center px-16 py-20 w-full text-center bg-neutral-800 max-md:px-5 max-md:max-w-full">
         <div className="flex flex-col mt-11 mb-7 max-w-full w-[778px] max-md:mt-10">
           <h1 className="text-6xl font-extrabold text-white leading-[72px] max-md:max-w-full max-md:text-4xl max-md:leading-[58px]">
