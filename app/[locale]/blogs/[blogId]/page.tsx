7fc5ce6bc8ce4721365436bd8f3162d0207cc8ee
Minh Long <minhlong.wb@gmail.com>
2024-04-29 16:43:33 +0700

    [Blog Article] Header improvement

--- app/[locale]/blogs/[blogId]/page.tsx
+++ app/[locale]/blogs/[blogId]/page.tsx
@@ -1,15 +1,31 @@
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
+
 const BlogArticle = () => {
   return (
     <main className="bg-[#12141D]">
-      <section className="bg-[url('/blogs/blog-article-header.png')] text-white h-[40vh] bg-center bg-no-repeat">
-        doushitaraiidesuke
+      <section className="bg-[linear-gradient(to_bottom,rgba(0,0,0,0),rgba(0,0,0,0.9)),url('/blogs/blog-article-header.png')] text-white h-[40vh] bg-center bg-no-repeat flex flex-col justify-end items-center">
+        <div className="max-w-screen-lg flex justify-between w-full mb-10 px-5">
+          <div className="flex py-4 px-8 bg-[#808080] bg-opacity-50 rounded-full">
+            <span className="text-[#808080] font-medium">News</span>
+            <ChevronRightIcon
+              className="text-[#808080] mx-1 font-bold"
+              width={16}
+            />
+            <span className="text-[#808080] opacity-50">Design</span>
+          </div>
+          <div className="flex">
+            <img
+              loading="lazy"
+              className="rounded-full w-14"
+              src={"/blogs/author-image.png"}
+            />
+            <div className="flex flex-col justify-center pl-3">
+              <p className="font-medium">Sergei Filatov</p>
+              <p className="text-[#B0B7C3] font-medium">14th May</p>
+            </div>
+          </div>
+        </div>
       </section>
-      {/* <Image
-        src="/blogs/blog-article-header.png"
-        alt="headerimg"
-        width={69}
-        height={69}
-      /> */}
     </main>
   );
 };
