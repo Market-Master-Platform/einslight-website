7536584fff4f24ec340cb4b2544e5b7de4c3296b
Minh Long <minhlong.wb@gmail.com>
2024-04-29 14:15:46 +0700

    [Blog Article] Recenter header image

--- app/[locale]/blogs/[blogId]/page.tsx
+++ app/[locale]/blogs/[blogId]/page.tsx
@@ -1,5 +1,17 @@
 const BlogArticle = () => {
-  return <div>Blog article page placeholder</div>;
+  return (
+    <main className="bg-[#12141D]">
+      <section className="bg-[url('/blogs/blog-article-header.png')] text-white h-[40vh] bg-center bg-no-repeat">
+        doushitaraiidesuke
+      </section>
+      {/* <Image
+        src="/blogs/blog-article-header.png"
+        alt="headerimg"
+        width={69}
+        height={69}
+      /> */}
+    </main>
+  );
 };
 
 export default BlogArticle;
