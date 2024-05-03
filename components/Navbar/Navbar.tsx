e6621be93c80cfb12295a55eb62985a015d19921
Minh Long <minhlong.wb@gmail.com>
2024-05-03 23:18:30 +0700

    [Header] Make the items' font lighter

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -61,7 +61,7 @@ const Navbar: React.FC<NavbarProps> = ({
   return (
     <React.Fragment>
       <div className="fixed w-full top-0 z-[51]">
-        <header className="flex gap-5 justify-between px-10 max-md:px-5 py-3 text-lg font-bold leading-8 text-white bg-zinc-900">
+        <header className="flex gap-5 justify-between px-10 max-md:px-5 py-3 text-lg font-semibold leading-8 text-white bg-zinc-900">
           <CustomImage
             src="/static/images/company-logo.svg"
             alt="Company logo"
