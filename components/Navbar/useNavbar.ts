c71b667ab151a516feda2c9340ea85626b9f9236
Minh Long <minhlong.wb@gmail.com>
2024-05-05 01:11:53 +0700

    [Header] Change language without localstorage

--- components/Navbar/useNavbar.ts
+++ components/Navbar/useNavbar.ts
@@ -1,15 +1,34 @@
+"use client";
+import { usePathname } from "next/navigation";
 import { useDictionary } from "@/context/dictionary-provider";
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 
 export interface NavItemValues {
   text: string;
   hasSubItems?: boolean;
   href?: string;
   onClick?: () => void;
+  forLang?: boolean;
 }
 
 const useNavbar = () => {
   const [isOpenNavbarMenu, setOpenNavbarMenu] = useState<boolean>(false);
+  // const [currentLang, setCurrentLang] = useState<string | null>("");
+  const [langPathname, setLangPathname] = useState<string>("");
+  const pathname: string = usePathname();
+
+  useEffect(() => {
+    // const lang: string | null = localStorage.getItem("lang");
+    // setCurrentLang(lang);
+    let splittedPathname: string[] = pathname.split("/");
+    if (splittedPathname[1] === "en") {
+      splittedPathname[1] = "vi";
+      setLangPathname(splittedPathname.join("/"));
+    } else {
+      splittedPathname[1] = "en";
+      setLangPathname(splittedPathname.join("/"));
+    }
+  }, []);
 
   const handleToggleNavbarMenu = () => {
     setOpenNavbarMenu((currentState) => !currentState);
@@ -29,6 +48,10 @@ const useNavbar = () => {
     {
       text: dictionary.navbar.contact,
       href: `/en/message`,
+    },
+    {
+      text: "EN/VI",
+      href: langPathname,
     },
   ];
 
